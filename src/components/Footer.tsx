9eab66f32f06cdb106e8139d3bc3fff8bddfdce0
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-11 21:34:09 +0100

    Updated Footer.tsx, passed tests

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer role="footer">Footer</footer>
+    <footer role="footer">
+      <h3>Coded by 
+        <a href="https://www.lunasmithart.com/" target='_blank' className='LunaSmith'>Luna Smith</a>
+        , open-source on 
+        <a href="https://github.com/Lu-Smith/vitest-react-choose-lotto-numbers-app" target='_blank' className='GitHub'>GitHub</a>
+        .
+      </h3>
+    </footer>
   )
 }
 
