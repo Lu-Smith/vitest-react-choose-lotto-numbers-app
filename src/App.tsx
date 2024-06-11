2ba761217edd2a5f8b5a8e86593e9dbd1a7f15f6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-11 20:13:25 +0100

    Updated styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
   return (
     <>
       <div>
-        Hello World
+        <h1>Hello World</h1>
+        <h2>GoodBye</h2>
       </div>
     </>
   )
