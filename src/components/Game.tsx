f55bdd983d2e6a592252410980892c9ec2ec29c6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-12 21:42:52 +0100

    Updated Game.tsx, passed test

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
+import Machine from '../assets/MachinesSpreadsheet.png'
 
 const Game = () => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+
+    if (canvas) {
+      const ctx = canvas.getContext('2d');
+      const img = new Image();
+      img.src = Machine;
+
+      canvas.width = 644/2;
+      canvas.height = 772/2;
+
+      if (ctx) {
+        img.onload = () => {
+          ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        }
+      }
+    }
+    
+  }, []);
+
   return (
-    <canvas role='canvas'></canvas>
+    <>
+      <canvas role='canvas' ref={canvasRef}></canvas>
+    </>
+    
   )
 }
 
