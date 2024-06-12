50fff645f4378f8594caabc68c58967bb9fe0b51
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-12 22:41:19 +0100

    Updated Game.tsx

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useRef } from 'react';
-import Machine from '../assets/MachinesSpreadsheet.png'
+import React, { useEffect, useRef, useState } from 'react';
+import Machine from '../assets/MachinesSpreadsheet.png';
 
-const Game = () => {
+const Game: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [play, setPlay] = useState(false);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -15,18 +16,50 @@ const Game = () => {
       canvas.width = 644/2;
       canvas.height = 772/2;
 
-      if (ctx) {
-        img.onload = () => {
-          ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+      const spriteWidth = 644;
+      const spriteHeight = 772;
+
+      let animationFrame: number | null = null;
+      let frameX = 0;
+      const frameRate = 150;
+
+      ctx?.drawImage(img, frameX * spriteWidth, 0, spriteWidth, spriteHeight, 0, 0, canvas.width, canvas.height);
+
+      const drawFrame = () => {
+        if (ctx && img.complete) {
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
+          ctx.drawImage(img, frameX * spriteWidth, 0, spriteWidth, spriteHeight, 0, 0, canvas.width, canvas.height);
         }
+      };
+      const animate = () => {
+        drawFrame();
+        frameX++;
+        if (frameX > 6) {
+          frameX = 0;
+          setPlay(false);
+          cancelAnimationFrame(animationFrame!);
+        } else {
+          setTimeout(() => {
+            animationFrame = requestAnimationFrame(animate);
+          }, frameRate);
+        }
+      };
+
+      if (play) {
+        animate();
       }
+
+      return () => {
+        cancelAnimationFrame(animationFrame!);
+      };
     }
     
-  }, []);
+  }, [play]);
 
   return (
     <>
       <canvas role='canvas' ref={canvasRef}></canvas>
+      <button onClick={() => setPlay(true)}>Play</button>
     </>
     
   )
