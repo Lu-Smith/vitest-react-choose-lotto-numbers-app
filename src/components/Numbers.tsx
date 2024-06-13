2978bfc275203f3913271bf4dabb40a3b9714b23
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-13 17:05:41 +0100

    Updated Numbers.tsx

--- src/components/Numbers.tsx
+++ src/components/Numbers.tsx
@@ -4,6 +4,11 @@ const Numbers: React.FC = () => {
   return (
     <div data-testid='numbers' className='numbers'>
       <h2>1</h2>
+      <h2><span>12</span></h2>
+      <h2>23</h2>
+      <h2><span>34</span></h2>
+      <h2>42</h2>
+      <h2><span>50</span></h2>
     </div>
   )
 }
