d1c0b9c0075e447bbef567cdbae5e2b4739c0af3
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-13 17:05:31 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
+import { useState } from 'react';
 import Footer from './components/Footer'
 import Game from './components/Game'
 
 const App = () => {
+  const [reset, setReset] = useState(false);
+
+  const handleReset = () => {
+    setReset(prev => !prev);
+  }
 
   return (
     <>
       <div className='App'>
-        <h1>
-          Choose your{' '} 
-          <span>lucky</span>
-          {' '} numbers.
-        </h1>
-        <Game />
+        {reset ?
+          <div className='question'>
+            <h2>Are these <span>numbers</span> calling <span>your</span> name?</h2>
+          </div>
+        :
+          <h1>
+            Choose your{' '} 
+            <span>lucky</span>
+            {' '} numbers.
+          </h1>
+        }
+        <Game handleReset={handleReset} />
         <Footer />
       </div>
     </>
